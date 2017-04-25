82d738d957b215c09879a20f6ad2418ec8e944c1
Juan Carlos <juan.deguer@gmail.com>
2017-04-25 15:11:51 -0300

    Create install.pp

---
+++ manifests/install.pp
@@ -0,0 +1,13 @@
+# = Class: elasticsearch::install
+#
+# This class installs the application.
+#
+class elasticsearch::install inherits elasticsearch::params {
+
+  $package_elasticsearch_version        = $::elasticsearch::params::package_elasticsearch_version
+  $package_ensure                       = $::elasticsearch::params::package_ensure
+
+  package { $package_elasticsearch_version:
+    ensure => $package_ensure,
+  }
+}
